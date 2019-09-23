bce21f253df8f295d951952268d70d232eedd6df
Josh Steele <josh.steele@jhuapl.edu>
2019-09-23 14:32:54 -0400

    Adding comments, cleaning up the custom spectra classes

---
+++ src/edu/jhuapl/sbmt/core/listeners/SearchProgressListener.java
@@ -0,0 +1,30 @@
+package edu.jhuapl.sbmt.core.listeners;
+
+/**
+ * Interface for sending back information on the progress of a search.  It doesn't necessarily need to be used with a UI elements, but that was where it was originally used.
+ * @author steelrj1
+ *
+ */
+public interface SearchProgressListener
+{
+	/**
+	 * The search has started
+	 */
+	public void searchStarted();
+
+	/**
+	 * The search is now <pre>percentComplete</pre> of 100%.
+	 * @param percentComplete
+	 */
+	public void searchProgressChanged(int percentComplete);
+
+	/**
+	 * The search has ended.
+	 */
+	public void searchEnded();
+
+	/**
+	 * This search is indeterminate; setup something like a progress bar with an indeterminate state to show this.
+	 */
+	public void searchIndeterminate();
+}
