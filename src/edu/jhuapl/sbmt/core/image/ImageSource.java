8e97f854e888be1e931768244901911aecfd2503
James Peachey <jpeachey@users.noreply.github.com>
2022-10-12 10:23:41 -0400

    RM 2356: add getPointingDir() method to locate pointing files.

--- src/edu/jhuapl/sbmt/core/image/ImageSource.java
+++ src/edu/jhuapl/sbmt/core/image/ImageSource.java
@@ -2,26 +2,32 @@ package edu.jhuapl.sbmt.core.image;
 
 public enum ImageSource
 {
-    SPICE("SPICE Derived", "pds"),
-    GASKELL("SPC Derived", "gaskell"),
-    GASKELL_UPDATED("SPC Derived", "gaskell_updated"),
-    LABEL("Label Derived", "label"),
-    CORRECTED("Corrected", "corrected"),
-    CORRECTED_SPICE("Corrected SPICE Derived", "corrected_pds"),
-    IMAGE_MAP("ImageMap", "image_map"),
-    LOCAL_CYLINDRICAL("LocalCylindrical", "local_cylindrical"),
-    LOCAL_PERSPECTIVE("LocalPerspective", "local_perspective"),
-    FALSE_COLOR("FalseColor", "false_color");
+    SPICE("SPICE Derived", "pds", "infofiles"), //
+    GASKELL("SPC Derived", "gaskell", "sumfiles"), //
+    GASKELL_UPDATED("SPC Derived", "gaskell_updated", "sumfiles-updated"), //
+    LABEL("Label Derived", "label", "labels"), //
+    CORRECTED("Corrected", "corrected", "sumfiles-corrected"), //
+    CORRECTED_SPICE("Corrected SPICE Derived", "corrected_pds", "infofiles-corrected"), //
+    IMAGE_MAP("ImageMap", "image_map", ""), //
+    LOCAL_CYLINDRICAL("LocalCylindrical", "local_cylindrical", ""), //
+    LOCAL_PERSPECTIVE("LocalPerspective", "local_perspective", ""), //
+    FALSE_COLOR("FalseColor", "false_color", "");
 
-    private String string; //String used in the GUI Pointing drop-down menu
-    private String databaseTableName; //String used in the database table name
+    // String used in the GUI Pointing drop-down menu
+    private final String string;
+    // String used in the database table name
+    private final String databaseTableName;
+    // String locating pointing files under the model/imager directory
+    private final String pointingDir;
 
-    private ImageSource(String nameString, String databaseTableName)
+    private ImageSource(String nameString, String databaseTableName, String pointingDir)
     {
         this.string = nameString;
         this.databaseTableName = databaseTableName;
+        this.pointingDir = pointingDir;
     }
 
+    @Override
     public String toString()
     {
         return string;
@@ -30,6 +36,18 @@ public enum ImageSource
     public String getDatabaseTableName()
     {
         return databaseTableName;
+    }
+
+    /**
+     * Return the directory used to store pointing files for the relevant source
+     * type. The path is assumed to be relative to the top model-specific imager
+     * directory.
+     *
+     * @return the pointing directory name
+     */
+    public String getPointingDir()
+    {
+        return pointingDir;
     }
 
     public static String printSources(int tabLen)
@@ -43,7 +61,6 @@ public enum ImageSource
             i++;
         }
         ImageSource[] sources = ImageSource.values();
-        String imagesource = new String();
         for (ImageSource s : sources)
         {
             out = out + tab + s.name() + "\n";
