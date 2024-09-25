ec41c25338b4a379a4125f9e583f4d1545fa4af2
Josh Steele <josh.steele@jhuapl.edu>
2024-09-25 17:03:24 -0400

    Fixed a change that should not have been here, updated pom

--- src/edu/jhuapl/sbmt/core/body/SmallBodyModel.java
+++ src/edu/jhuapl/sbmt/core/body/SmallBodyModel.java
@@ -1,19 +1,15 @@
 package edu.jhuapl.sbmt.core.body;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import edu.jhuapl.saavtk.config.IBodyViewConfig;
-import edu.jhuapl.saavtk.model.GenericPolyhedralModel;
-import edu.jhuapl.saavtk.model.LidarDataSource;
-import edu.jhuapl.saavtk.util.MapUtil;
-import edu.jhuapl.sbmt.core.config.ISmallBodyViewConfig;
-import edu.jhuapl.sbmt.core.config.Instrument;
 import vtk.vtkPolyData;
 import vtk.vtkTransform;
 import vtk.vtkTransformFilter;
+
+import edu.jhuapl.saavtk.config.IBodyViewConfig;
+import edu.jhuapl.saavtk.model.GenericPolyhedralModel;
+import edu.jhuapl.sbmt.core.config.ISmallBodyViewConfig;
+import edu.jhuapl.sbmt.core.config.Instrument;
 
 public class SmallBodyModel extends GenericPolyhedralModel implements ISmallBodyModel
 {
@@ -24,8 +20,6 @@ public class SmallBodyModel extends GenericPolyhedralModel implements ISmallBody
             SlopeUnitsStr, ElevUnitsStr, GravAccUnitsStr, GravPotUnitsStr
     };
     private static final ColoringValueType DEFAULT_COLORING_VALUE_TYPE = ColoringValueType.CELLDATA;
-    
-    private List<LidarDataSource> lidarDataSourceL = new ArrayList<>();
 
 	public ISmallBodyViewConfig getSmallBodyConfig()
     {
@@ -94,10 +88,6 @@ public class SmallBodyModel extends GenericPolyhedralModel implements ISmallBody
             boolean lowestResolutionModelStoredInResource)
     {
         super(config, modelNames, modelFiles, coloringFiles, coloringNames, coloringUnits, coloringHasNulls, coloringValueType, lowestResolutionModelStoredInResource);
-    	if (!getConfig().isCustomTemporary())
-        {
-            loadCustomLidarDataSource();
-        }
     }
 
     protected void initializeConfigParameters(
@@ -154,53 +144,6 @@ public class SmallBodyModel extends GenericPolyhedralModel implements ISmallBody
 
     	vtkPolyData polydata = transformFilter.GetPolyDataOutput();
     	setSmallBodyPolyDataAtPosition(polydata);
-    }
-    
-    public void loadCustomLidarDataSource()
-    {
-        lidarDataSourceL = new ArrayList<>();
-
-        String configFilename = getConfigFilename();
-
-        if (!(new File(configFilename).exists()))
-            return;
-
-        MapUtil configMap = new MapUtil(configFilename);
-
-        convertOldConfigFormatToNewVersion(configMap);
-
-		if (configMap.containsKey(GenericPolyhedralModel.LIDAR_DATASOURCE_NAMES)
-				&& configMap.containsKey(GenericPolyhedralModel.LIDAR_DATASOURCE_NAMES))
-        {
-            String[] nameArr = configMap.get(GenericPolyhedralModel.LIDAR_DATASOURCE_NAMES).split(",", -1);
-            String[] pathArr = configMap.get(GenericPolyhedralModel.LIDAR_DATASOURCE_PATHS).split(",", -1);
-
-            for (int i = 0; i < nameArr.length; ++i)
-            {
-                LidarDataSource info = new LidarDataSource(nameArr[i], pathArr[i]);
-                lidarDataSourceL.add(info);
-            }
-        }
-    }
-    
-    public void addCustomLidarDataSource(LidarDataSource info) throws IOException
-    {
-        lidarDataSourceL.add(info);
-    }
-
-    public void setCustomLidarDataSource(int index, LidarDataSource info) throws IOException
-    {
-        lidarDataSourceL.set(index, info);
-    }
-
-    public void removeCustomLidarDataSource(int index) throws IOException
-    {
-        lidarDataSourceL.remove(index);
-    }
-
-    public List<LidarDataSource> getLidarDataSourceList()
-    {
-        return lidarDataSourceL;
     }
 
 }
